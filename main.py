699ae33921a75c1387773048d2cecacd12107c06
anandhukrishna1998 <63602359+anandhukrishna1998@users.noreply.github.com>
2025-01-24 20:37:31 +0100

    completed data transformation

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import sys
-import os
-
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
-# from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
-# from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
+#from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 # from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
@@ -19,16 +16,16 @@ except Exception as e:
     raise e
 
 
-# STAGE_NAME="Data Transformation stage"
+STAGE_NAME="Data Transformation stage"
 
-# try:
-#     logger.info(f"stage {STAGE_NAME} initiated")
-#     data_ingestion_pipeline=DataTransformationTrainingPipeline()
-#     data_ingestion_pipeline.initiate_data_transformation()
-#     logger.info(f"Stage {STAGE_NAME} Completed")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 
